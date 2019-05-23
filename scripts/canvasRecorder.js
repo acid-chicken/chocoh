a2fcb31b3f67b5e30c86ff7dbee50c67dc0aa89c
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2019-05-24 04:34:16 +0900

    Support screen shot command

--- scripts/canvasRecorder.js
+++ scripts/canvasRecorder.js
@@ -57,11 +57,74 @@
     document.body.classList.remove('record')
   }
 
+  const shot = () => {
+    const [canvas] = document.getElementsByTagName('canvas')
+
+    if (!canvas) {
+      return
+    }
+
+    const chunks = []
+
+    const push = e => {
+      if (e.data) {
+        chunks.push(e.data)
+
+        recorder.stop()
+      }
+    }
+
+    const pull = e => {
+      push(e)
+
+      const canvas = document.createElement('canvas')
+
+      const video = document.createElement('video')
+
+      video.src = URL.createObjectURL(new Blob(chunks.splice(0)))
+
+      video.autoplay = true
+
+      const listener = e => {
+        video.pause()
+
+        canvas.width = video.width = video.videoWidth
+
+        canvas.height = video.height = video.videoHeight
+
+        const context = canvas.getContext('2d')
+
+        context.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+        const a = document.createElement('a')
+
+        a.download = Date.now() + '.png'
+
+        a.href = canvas.toDataURL()
+
+        a.dispatchEvent(new MouseEvent('click', {
+          bubbles: false
+        }))
+      }
+
+      video.addEventListener('loadeddata', listener)
+    }
+
+    const recorder = new MediaRecorder(canvas.captureStream())
+
+    recorder.addEventListener('dataavailable', push)
+
+    recorder.addEventListener('stop', pull)
+
+    recorder.start(1)
+  }
+
   const listener = e =>
     (/^(Mac|Apple|i[A-Z])/.test(navigator.platform) ? e.metaKey : e.ctrlKey) &&
     e.altKey &&
     !(e.code === 'Comma' ? start() :
-      e.code === 'Period' ? stop() : true) &&
+      e.code === 'Period' ? stop() :
+      e.code === 'Slash' ? shot() : true) &&
     e.preventDefault()
 
   addEventListener('keydown', listener, true)
