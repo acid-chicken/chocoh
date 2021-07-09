64854bba901b714250e989cc536f57e454939ecb
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2021-07-09 21:14:19 +0000

    Update assetImagesRedirector.js

--- scripts/assetImagesRedirector.js
+++ scripts/assetImagesRedirector.js
@@ -7,14 +7,18 @@
 
   const modeKey = prefix + 'mode'
 
+  const firstattrKey = prefix + 'firstattr'
+  
+  const host = ['fun', 'azne', 'srolocynihs'].reduce((a, c) => Array.prototype.reduce.call(c, (a, c) => c + a) + '.' + a)
+
   const onBeforeRequest = e => ~~localStorage.getItem(enableKey) && localStorage.getItem(modeKey) !== 'none' ? {
-    redirectUrl: e.url.replace('//shinycolors.enza.fun/assets/', `//shinycolors.enza.fun/chocoh/images/${localStorage.getItem(modeKey)}/`)
+    redirectUrl: e.url.replace(`//${host}/assets/`, `//${host}/chocoh/images/${localStorage.getItem(modeKey)}${localStorage.getItem(firstattrKey)}/`)
   } : {
   }
 
   const filter = {
     urls: [
-      'https://shinycolors.enza.fun/assets/*'
+      `https://${host}/assets/*`
     ],
     types: [
       'image'
